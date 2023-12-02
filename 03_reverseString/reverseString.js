86bf9bdc1370eccbd771714a9a7071972cd4aa53
urostodorovic95 <150077866+urostodorovic95@users.noreply.github.com>
2023-12-02 21:43:25 +0100

    complete 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
 const reverseString = function(string) {
-    
+    const mapped = [];
+    for (let char of string) {
+        mapped.push(char)
+    }
+    return (
+        mapped.reverse().join("")
+    )
 };
 
 // Do not edit below this line
